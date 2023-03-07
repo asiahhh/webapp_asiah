e14b6bcb4808b99b5f3ab2e5cea361b570d12f82
asiahhh <>
2023-03-07 08:03:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    name = None
+    gender = None
+    hobbies = None
+    feedback = None
+
+
+
+
+  
+  if request.method['name']
+    
+app.run(host='0.0.0.0', port=81)
