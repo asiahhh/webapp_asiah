32cf79be4d5946bb4f5088f44ddf0c4c91c17486
asiahhh <125533782+asiahhh@users.noreply.github.com>
2023-03-07 09:07:29 +0000

    updated html, css

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
-    gender = None
-    hobbies = None
-    feedback = None
-
-
-
-
+  numbers = [1, 2, 3, 4, 5]
+  with open('content.txt', 'r') as file:
+    intro = file.read()
+  data = None
+  if request.method == 'POST':
+    data = request.form['data']
+    data = data.upper() # change to upper case
+    with open('whatever.txt', 'a') as file:
+      file.write(data + '\n')
+  return render_template('index.html', data=data, intro=intro) # send back to html
   
-  if request.method['name']
-    
+   
 app.run(host='0.0.0.0', port=81)
